ec9edd4a7f0d2e63e417849ab42aface9b5684c3
Mo <646309547@qq.com>
2017-06-03 17:02:50 +0800

    完成了草的增殖

--- content/assets/js/move.js
+++ content/assets/js/move.js
@@ -1,6 +1,7 @@
 var animal = {
     data : data_map["forest"],
     size : 0,
+
     find:function(){
         var table = document.querySelector("#map");
         var tr = $("#map").children("tr")
@@ -17,8 +18,10 @@ var animal = {
                             if( td.eq(j).attr("title")==0){
                                 td.eq(j).removeClass("material sleep" ).addClass("material air");
                             }else if(td.eq(j).attr("title")>=60){
-                                td.eq(j+1).removeClass("material air" ).addClass("material sleep");
-                                td.eq(j+1).attr("title",30);td.eq(j).attr("title",30);
+                                if(td.eq(j+1).attr("class")=="material air"){
+                                    td.eq(j+1).removeClass("material air" ).addClass("material sleep");
+                                    td.eq(j+1).attr("title",30);td.eq(j).attr("title",30);
+                                }
                             }
                             else{
                                 td.eq(j).removeClass("material sleep" ).addClass("material air");
@@ -31,8 +34,8 @@ var animal = {
                         else if(td.eq(j-1).attr("class")=="material grass"){
                             td.eq(j).removeClass("material sleep" ).addClass("material air");
                             td.eq(j-1).removeClass("material grass" ).addClass("material sleep");
-                            td.eq(j-1).attr("title",td.eq(j).attr("title")+5);
-                             td.eq(j).attr("title",0);
+                            td.eq(j-1).attr("title",parseInt(td.eq(j).attr("title"))+10);
+                            td.eq(j).attr("title",0);
                         }
                     }
                     else if(1<type&&type<=2){
@@ -40,18 +43,20 @@ var animal = {
                             if( td.eq(j).attr("title")==0){
                                 td.eq(j).removeClass("material sleep" ).addClass("material air");
                             }else if(td.eq(j).attr("title")>=60){
-                                td.eq(j+1).removeClass("material air" ).addClass("material sleep");
-                                td.eq(j+1).attr("title",30);td.eq(j).attr("title",30);
+                                if(td.eq(j+1).attr("class")=="material air"){
+                                    td.eq(j+1).removeClass("material air" ).addClass("material sleep");
+                                    td.eq(j+1).attr("title",30);td.eq(j).attr("title",30);
+                                }
                             }else{
                                 td.eq(j).removeClass("material sleep" ).addClass("material air");
                                 td.eq(j+1).removeClass("material air" ).addClass("material sleep");
-                                td.eq(j+1).attr("title",td.eq(j).attr("title"));
+                                td.eq(j+1).attr("title",td.eq(j).attr("title")-1);
                                 td.eq(j).attr("title",0);
                             }
                         }else if(td.eq(j+1).attr("class" )=="material grass"){
                             td.eq(j).removeClass("material sleep" ).addClass("material air");
                             td.eq(j+1).removeClass("material grass" ).addClass("material sleep");
-                            td.eq(j+1).attr("title",td.eq(j).attr("title"));
+                            td.eq(j+1).attr("title",parseInt(td.eq(j).attr("title"))+10);
                             td.eq(j).attr("title",0);
                         }
                     }
@@ -61,19 +66,21 @@ var animal = {
                              if( td.eq(j).attr("title")==0){
                                 td.eq(j).removeClass("material sleep" ).addClass("material air");
                             }else if(td.eq(j).attr("title")>=60){
-                                td.eq(j+1).removeClass("material air" ).addClass("material sleep");
-                                td.eq(j+1).attr("title",30);td.eq(j).attr("title",30);
+                                if(td.eq(j+1).attr("class")=="material air"){
+                                    td.eq(j+1).removeClass("material air" ).addClass("material sleep");
+                                    td.eq(j+1).attr("title",30);td.eq(j).attr("title",30);
+                                }
                             }else{
                                 td.eq(j).removeClass("material sleep" ).addClass("material air");
                                 td2.eq(j).removeClass("material air" ).addClass("material sleep");
-                                td2.eq(j).attr("title",td.eq(j).attr("title"));
+                                td2.eq(j).attr("title",td.eq(j).attr("title")-1);
                                 td.eq(j).attr("title",0);
                             }
                         }
                         else if(td2.eq(j).attr("class" )=="material grass"){
                             td.eq(j).removeClass("material sleep" ).addClass("material air");
                             td2.eq(j).removeClass("material grass" ).addClass("material sleep");
-                            td2.eq(j).attr("title",td.eq(j).attr("title"));
+                            td2.eq(j).attr("title",parseInt(td.eq(j).attr("title"))+10);
                             td.eq(j).attr("title",0);
                         }
                     }
@@ -82,23 +89,51 @@ var animal = {
                         if(td2.eq(j).attr("class" )=="material air"){
                             if( td.eq(j).attr("title")==0){
                                 td.eq(j).removeClass("material sleep" ).addClass("material air");
-                            }else if(td.eq(j).attr("title")>=60){
-                                td.eq(j+1).removeClass("material air" ).addClass("material sleep");
-                                td.eq(j+1).attr("title",30);td.eq(j).attr("title",30);
+                            }else if(td.eq(j).attr("title")>=80){
+                               if(td.eq(j+1).attr("class")=="material air"){
+                                    td.eq(j+1).removeClass("material air" ).addClass("material sleep");
+                                    td.eq(j+1).attr("title",30);td.eq(j).attr("title",30);
+                                }
                             }else{
                                 td.eq(j).removeClass("material sleep" ).addClass("material air");
                                 td2.eq(j).removeClass("material air" ).addClass("material sleep");
-                                td2.eq(j).attr("title",td.eq(j).attr("title"));
+                                td2.eq(j).attr("title",td.eq(j).attr("title")-1);
                                 td.eq(j).attr("title",0);
                             }
                         }
                         else if(td2.eq(j).attr("class" )=="material grass"){
                             td.eq(j).removeClass("material sleep" ).addClass("material air");
                             td2.eq(j).removeClass("material grass" ).addClass("material sleep");
-                            td2.eq(j).attr("title",td.eq(j).attr("title"));
+                            td2.eq(j).attr("title",parseInt(td.eq(j).attr("title"))+10);
                             td.eq(j).attr("title",0);
                         }
                     }
+                }
+                if(history_income_type =="material grass"){
+                    var td2 = tr.eq(i-1).find("td");
+                    var td3 = tr.eq(i+1).find("td");
+                    if(td.eq(j).attr("title")>=20){
+                        if(td.eq(j-1).attr("class")=="material air"){
+                            td.eq(j-1).removeClass("material air" ).addClass("material grass");
+                            td.eq(j).attr("title",0);td.eq(j+1).attr("title",0);
+                        }else if(td.eq(j+1).attr("class")=="material air"){
+                            td.eq(j+1).removeClass("material air" ).addClass("material grass");
+                            td.eq(j).attr("title",0);td.eq(j+1).attr("title",0);
+                        }else if(td2.eq(j).attr("class")=="material air"){
+                            td2.eq(j).removeClass("material air" ).addClass("material grass");
+                            td.eq(j).attr("title",0);td2.eq(j).attr("title",0);
+                        }else if(td3.eq(j).attr("class")=="material air"){
+                            td3.eq(j).removeClass("material air" ).addClass("material grass");
+                            td.eq(j).attr("title",0);td3.eq(j).attr("title",0);
+                        }else{
+                             td.eq(j).attr("title",5);
+                        }
+                    }else{
+                        td.eq(j).attr("title",parseInt(td.eq(j).attr("title"))+1);
+                    }
+                }
+                if(history_income_type =="grass material sleep"){
+                     td.eq(j).removeClass("grass material sleep" ).addClass(" material sleep");
                 }
             }
         }
