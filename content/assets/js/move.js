60092467982d8194bae5875c1ddb1058ba617c6e
Mo <646309547@qq.com>
2017-06-03 16:21:35 +0800

    增加了绵羊的增殖以及死亡

--- content/assets/js/move.js
+++ content/assets/js/move.js
@@ -12,45 +12,91 @@ var animal = {
                     var type = Math.random()*4;
                     // var type = 2
                     if(0<type&&type<=1){
+        
                         if( td.eq(j-1).attr("class" )=="material air"){
-                            td.eq(j).attr("title","1");
-                            td.eq(j).removeClass("material sleep" ).addClass("material air");
-                            td.eq(j-1).removeClass("material air" ).addClass("material sleep");
+                            if( td.eq(j).attr("title")==0){
+                                td.eq(j).removeClass("material sleep" ).addClass("material air");
+                            }else if(td.eq(j).attr("title")>=60){
+                                td.eq(j+1).removeClass("material air" ).addClass("material sleep");
+                                td.eq(j+1).attr("title",30);td.eq(j).attr("title",30);
+                            }
+                            else{
+                                td.eq(j).removeClass("material sleep" ).addClass("material air");
+                                td.eq(j-1).removeClass("material air" ).addClass("material sleep");
+                                td.eq(j-1).attr("title",td.eq(j).attr("title")-1);
+                                td.eq(j).attr("title",0);
+                            }
+                           
                         }    
                         else if(td.eq(j-1).attr("class")=="material grass"){
                             td.eq(j).removeClass("material sleep" ).addClass("material air");
                             td.eq(j-1).removeClass("material grass" ).addClass("material sleep");
+                            td.eq(j-1).attr("title",td.eq(j).attr("title")+5);
+                             td.eq(j).attr("title",0);
                         }
                     }
                     else if(1<type&&type<=2){
                         if( td.eq(j+1).attr("class" )=="material air"){
-                            td.eq(j).removeClass("material sleep" ).addClass("material air");
-                            td.eq(j+1).removeClass("material air" ).addClass("material sleep");
+                            if( td.eq(j).attr("title")==0){
+                                td.eq(j).removeClass("material sleep" ).addClass("material air");
+                            }else if(td.eq(j).attr("title")>=60){
+                                td.eq(j+1).removeClass("material air" ).addClass("material sleep");
+                                td.eq(j+1).attr("title",30);td.eq(j).attr("title",30);
+                            }else{
+                                td.eq(j).removeClass("material sleep" ).addClass("material air");
+                                td.eq(j+1).removeClass("material air" ).addClass("material sleep");
+                                td.eq(j+1).attr("title",td.eq(j).attr("title"));
+                                td.eq(j).attr("title",0);
+                            }
                         }else if(td.eq(j+1).attr("class" )=="material grass"){
                             td.eq(j).removeClass("material sleep" ).addClass("material air");
                             td.eq(j+1).removeClass("material grass" ).addClass("material sleep");
+                            td.eq(j+1).attr("title",td.eq(j).attr("title"));
+                            td.eq(j).attr("title",0);
                         }
                     }
                     else if(2<type&&type<=3){
                         var td2 = tr.eq(i-1).find("td");
                         if(td2.eq(j).attr("class" )=="material air"){
-                            td.eq(j).removeClass("material sleep" ).addClass("material air");
-                            td2.eq(j).removeClass("material air" ).addClass("material sleep");
+                             if( td.eq(j).attr("title")==0){
+                                td.eq(j).removeClass("material sleep" ).addClass("material air");
+                            }else if(td.eq(j).attr("title")>=60){
+                                td.eq(j+1).removeClass("material air" ).addClass("material sleep");
+                                td.eq(j+1).attr("title",30);td.eq(j).attr("title",30);
+                            }else{
+                                td.eq(j).removeClass("material sleep" ).addClass("material air");
+                                td2.eq(j).removeClass("material air" ).addClass("material sleep");
+                                td2.eq(j).attr("title",td.eq(j).attr("title"));
+                                td.eq(j).attr("title",0);
+                            }
                         }
                         else if(td2.eq(j).attr("class" )=="material grass"){
                             td.eq(j).removeClass("material sleep" ).addClass("material air");
                             td2.eq(j).removeClass("material grass" ).addClass("material sleep");
+                            td2.eq(j).attr("title",td.eq(j).attr("title"));
+                            td.eq(j).attr("title",0);
                         }
                     }
                     else if(3<type&&type<=4 ){
                         var td2 = tr.eq(i+1).find("td");
                         if(td2.eq(j).attr("class" )=="material air"){
-                            td.eq(j).removeClass("material sleep" ).addClass("material air");
-                            td2.eq(j).removeClass("material air" ).addClass("material sleep");
+                            if( td.eq(j).attr("title")==0){
+                                td.eq(j).removeClass("material sleep" ).addClass("material air");
+                            }else if(td.eq(j).attr("title")>=60){
+                                td.eq(j+1).removeClass("material air" ).addClass("material sleep");
+                                td.eq(j+1).attr("title",30);td.eq(j).attr("title",30);
+                            }else{
+                                td.eq(j).removeClass("material sleep" ).addClass("material air");
+                                td2.eq(j).removeClass("material air" ).addClass("material sleep");
+                                td2.eq(j).attr("title",td.eq(j).attr("title"));
+                                td.eq(j).attr("title",0);
+                            }
                         }
                         else if(td2.eq(j).attr("class" )=="material grass"){
                             td.eq(j).removeClass("material sleep" ).addClass("material air");
                             td2.eq(j).removeClass("material grass" ).addClass("material sleep");
+                            td2.eq(j).attr("title",td.eq(j).attr("title"));
+                            td.eq(j).attr("title",0);
                         }
                     }
                 }
